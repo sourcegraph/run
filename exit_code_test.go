86c6128d8a75f6eec8807944288d90d9287d92ee
Robert Lin <robert@bobheadxi.dev>
2022-05-12 07:50:06 -0700

    doc: fixup ExitCoder, ExitCode docstrings and use run_test package (#13)

--- exit_code_test.go
+++ exit_code_test.go
@@ -1,23 +1,24 @@
-package run
+package run_test
 
 import (
 	"context"
 	"fmt"
 
 	"bitbucket.org/creachadair/shell"
+	"github.com/sourcegraph/run"
 )
 
 func ExampleExitCode() {
 	ctx := context.Background()
 
-	err := Cmd(ctx, "bash -c", shell.Quote("exit 123")).Run().Wait()
-	fmt.Println(ExitCode(err))
+	err := run.Cmd(ctx, "bash -c", shell.Quote("exit 123")).Run().Wait()
+	fmt.Println(run.ExitCode(err))
 
-	err = Cmd(ctx, "echo 'hello world!'").Run().Wait()
-	fmt.Println(ExitCode(err))
+	err = run.Cmd(ctx, "echo 'hello world!'").Run().Wait()
+	fmt.Println(run.ExitCode(err))
 
-	err = Cmd(ctx, "non-existing-binary").Run().Wait()
-	fmt.Println(ExitCode(err))
+	err = run.Cmd(ctx, "non-existing-binary").Run().Wait()
+	fmt.Println(run.ExitCode(err))
 	// Output:
 	// 123
 	// 0
