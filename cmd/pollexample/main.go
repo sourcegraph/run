151ad30c1a9e31ebf662e4196efc14c0258f4b89
Robert Lin <robert@bobheadxi.dev>
2022-05-16 16:58:50 -0700

    add cancellation example

--- cmd/pollexample/main.go
+++ cmd/pollexample/main.go
@@ -11,13 +11,15 @@ import (
 )
 
 func main() {
-	ctx := context.Background()
+	ctx, cancel := context.WithCancel(context.Background())
 
 	// Demonstrate that output streams live!
 	cmd := run.Bash(ctx, `for i in {1..10}; do echo -n "This is a test in loop $i "; date ; sleep 1; done`)
 	if err := cmd.Run().
 		Filter(func(ctx context.Context, line []byte, dst io.Writer) (int, error) {
 			if bytes.Contains(line, []byte("loop 3")) {
+				defer cancel()
+
 				written, err := run.Cmd(ctx, "echo", "Loop 3 detected, running a subcommand!").
 					Run().
 					WriteTo(dst)
