9f0a46a4365d62becd2066442312d99f56ba733f
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2022-10-31 19:02:55 +0200

    add BashWithOpts command for strict bash exec (#45)
    * add BashWithOpts command for strict bash exec
    
    Co-authored-by: Sander Ginn <sanderginn@users.noreply.github.com>
    Co-authored-by: Robert Lin <robert@bobheadxi.dev>
    Co-authored-by: Sander Ginn <sanderginn@users.noreply.github.com>
    Co-authored-by: Robert Lin <robert@bobheadxi.dev>

--- command.go
+++ command.go
@@ -10,6 +10,19 @@ import (
 	"bitbucket.org/creachadair/shell"
 )
 
+// BashOpt denotes options for running bash commands. For more options see 'man bash'
+type BashOpt string
+
+const (
+	// 'pipefail' instructs bash to fail an entire statement if any command in a pipefails.
+	BashOptPipeFail BashOpt = "pipefail"
+	// 'errexit' lets bash exit with an err exit code if a command fails .
+	BashOptErrExit BashOpt = "errexit"
+)
+
+// StrictBashOpts contains options that effectively enforce safe execution of bash commands.
+var StrictBashOpts = []BashOpt{BashOptPipeFail, BashOptErrExit}
+
 // Command builds a command for execution. Functions modify the underlying command.
 type Command struct {
 	ctx context.Context
@@ -38,6 +51,23 @@ func Cmd(ctx context.Context, parts ...string) *Command {
 		ctx:  ctx,
 		args: args,
 	}
+}
+
+// BashWith appends all the given bash options to the bash command with '-o'. The given parts
+// is then joined together to be executed with 'bash -c'
+//
+// The final command will have the following format: bash -o option-1 -c command. For recommended strict bash options
+// see StrictBashOpts, which has 'pipefail' and 'errexit' options
+func BashWith(ctx context.Context, opts []BashOpt, parts ...string) *Command {
+	var bash strings.Builder
+	bash.WriteString("bash")
+	for _, v := range opts {
+		bash.WriteString(" -o ")
+		bash.WriteString(string(v))
+	}
+	bash.WriteString(" -c")
+
+	return Cmd(ctx, bash.String(), Arg(strings.Join(parts, " ")))
 }
 
 // Bash joins all the parts and builds a command from it to be run by 'bash -c'.
